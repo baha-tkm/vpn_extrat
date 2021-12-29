9ac79454955d9ef3e6c0a0a81d37352f82925f25
Baha <96811703+baha-tkm@users.noreply.github.com>
2021-12-29 14:39:04 +0500

    Update import requests.py

--- import requests.py
+++ import requests.py
@@ -4,8 +4,10 @@ import lxml.html
 import csv
 
 def parse(url):
-
+try:
     api = requests.get(url)
+    except:
+        return
     tree = lxml.html.document_fromstring(api.text)
     text_original = tree.xpath('//*[@id="click_area"]/dev//*[@clas="original"]/text()')
     text_translate = tree.xpath('//*[@id="click_area"]/dev//*[@clas="translate"]/text()')
@@ -14,6 +16,9 @@ def parse(url):
         for i in range(len(text_original)):
             write.writerow(text_original[i])
             write.writerow(text_translate[i])
+
+def new_func():
+    return
 
 def main():
     parse("https:/www.amalgama-lab.com/song/t/tone_and_i/dance_monkey.html")
